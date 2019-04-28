5ec308e78d39cd27ab24633a9c9321d33c963528
mubashirk97 <30483326+mubashirk97@users.noreply.github.com>
2019-04-28 20:39:25 +0500

    Updated config file for Live website

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DBHOST', 'localhost');
-define('DBNAME', 'ecommerce');
-define('DBUSER', 'root');
-define('DBPASSWORD', '');
+define('DBNAME', 'id9438578_ecommerce');
+define('DBUSER', 'id9438578_root');
+define('DBPASSWORD', 'Root159');
 
 ?>
